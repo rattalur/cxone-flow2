39309910cb8ae797f222e64bfe57328cd269a7e1
nleach999 <nleach999@gmail.com>
2024-05-01 09:36:27 -0500

    add endpoint monikers

--- cxone_api/__init__.py
+++ cxone_api/__init__.py
@@ -60,6 +60,10 @@ class AuthEU(CxOneAuthEndpoint):
     def __init__(self, tenant_name):
         super().__init__(tenant_name, "eu.iam.checkmarx.net")
 
+class AuthDEU(CxOneAuthEndpoint):
+    def __init__(self, tenant_name):
+        super().__init__(tenant_name, "deu.iam.checkmarx.net")
+
 class AuthANZ(CxOneAuthEndpoint):
     def __init__(self, tenant_name):
         super().__init__(tenant_name, "anz.iam.checkmarx.net")
@@ -72,15 +76,21 @@ class AuthSingapore(CxOneAuthEndpoint):
     def __init__(self, tenant_name):
         super().__init__(tenant_name, "sng.iam.checkmarx.net")
 
+class AuthUAE(CxOneAuthEndpoint):
+    def __init__(self, tenant_name):
+        super().__init__(tenant_name, "mea.iam.checkmarx.net")
+        
 
 AuthRegionEndpoints = {
     "US" : AuthUS,
     "US2" : AuthUS2,
     "EU" : AuthEU,
     "EU2" : AuthEU,
+    "DEU" : AuthDEU,
     "ANZ" : AuthANZ,
     "India" : AuthIndia,
-    "Singapore" : AuthSingapore
+    "Singapore" : AuthSingapore,
+    "UAE" : AuthUAE
 }
 
 
@@ -107,6 +117,10 @@ class ApiEU2(CxOneApiEndpoint):
     def __init__(self):
         super().__init__("eu-2ast.checkmarx.net")
 
+class ApiDEU(CxOneApiEndpoint):
+    def __init__(self):
+        super().__init__("deu.checkmarx.net")
+
 class ApiANZ(CxOneApiEndpoint):
     def __init__(self):
         super().__init__("anz.ast.checkmarx.net")
@@ -119,15 +133,21 @@ class ApiSingapore(CxOneApiEndpoint):
     def __init__(self):
         super().__init__("sng.ast.checkmarx.net")
 
+class ApiUAE(CxOneApiEndpoint):
+    def __init__(self):
+        super().__init__("mea.ast.checkmarx.net")
+
 
 ApiRegionEndpoints = {
     "US" : ApiUS,
     "US2" : ApiUS2,
     "EU" : ApiEU,
     "EU2" : ApiEU2,
+    "DEU" : ApiDEU,
     "ANZ" : ApiANZ,
     "India" : ApiIndia,
-    "Singapore" : ApiSingapore
+    "Singapore" : ApiSingapore,
+    "UAE" : ApiUAE
 }
 
 
